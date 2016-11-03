24f26151cbe8525cf33662403f2d0c7015d567ce
shackledtodesk <andy.mayhew@gmail.com>
2016-11-03 08:50:35 +0000

    Fixed bug and added try/catch in gpsd.

--- piWriters/gpsd.py
+++ piWriters/gpsd.py
@@ -1,14 +1,20 @@
 from gps import *
 from time import sleep
 import threading
+import traceback
 
 class gpsdTime(threading.Thread):
   
   def __init__(self):
     threading.Thread.__init__(self)
-    self.gpsd = gps(mode=WATCH_ENABLE) #starting the stream of info
-    self.gpsd.next()
+    try:
+      self.gpsd = gps(mode=WATCH_ENABLE) #starting the stream of info
+    except:
+      traceback.print_exc()
+
     self.gpsd.running = True
+    self.gpsd.next()
+    ## Don't return from init until a Fix is made
     while self.gpsd.fix.mode != 3:
       self.gpsd.next()
 
@@ -21,7 +27,7 @@ class gpsdTime(threading.Thread):
     self.join()
       
   def getTime(self):
-    self.gpsd.next()
+    ##self.gpsd.next()
     return self.gpsd.utc
 
 if __name__ == '__main__':
